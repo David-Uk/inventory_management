a3ba203cf71b0b2608b0ebb3d170adabb4297f6d
David Ukelere <david.ukelere291@gmail.com>
2020-05-19 06:47:30 +0100

    Setup models for inventory app

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Device(models.Model):
+    type = models.CharField(max_length=100, blank=False)
+    price = models.IntegerField()
+    status = models.CharField(max_length=10, default='SOLD')
+    issues = models.CharField(max_length=100, default='No issues')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return 'Type: {0} Price :{1}'.format(self.type, self.price)
+
+
+class Laptop(Device):
+    pass
+
+
+class Desktop(Device):
+    pass
+
+
+class Mobile(Device):
+    pass
