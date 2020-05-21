3408efceb15ce9bd8697cdbb927700b5c7237975
David Ukelere <david.ukelere291@gmail.com>
2020-05-21 10:46:57 +0100

    Setup forms

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,9 @@ from .views import *
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^laptops$', display_laptops, name="display_laptops"),
+    url(r'^desktops$', display_desktops, name="display_desktops"),
+    url(r'^mobiles$', display_mobiles, name="display_mobiles"),
+    url(r'^add_laptop$', add_laptop, name='add_laptop'),
+    url(r'^add_desktop$', add_desktop, name='add_desktop'),
+    url(r'^add_mobile$', add_mobile, name='add_mobile')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect
+from .models import Laptop, Desktop, Mobile
+from .forms import *
 
 # Create your views here.
 
@@ -12,6 +13,48 @@ def display_laptops(request):
     items = Laptop.objects.all()
     context = {
         'items': items,
-
+        'header': 'Laptops'
     }
     return render(request, 'index.html', context)
+
+
+def display_desktops(request):
+    items = Desktop.objects.all()
+    context = {
+        'items': items,
+        'header': 'Desktops'
+    }
+    return render(request, 'index.html', context)
+
+
+def display_mobiles(request):
+    items = Mobile.objects.all()
+    context = {
+        'items': items,
+        'header': 'Mobiles'
+    }
+    return render(request, 'index.html', context)
+
+
+def add_device(request, cls):
+    if request.method == 'POST':
+        form = cls(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = LaptopForm()
+        return render(request, 'add_new.html', {'form': form})
+
+
+def add_laptop(request):
+    return add_device(request, LaptopForm)
+
+
+def add_desktop(request):
+    return add_device(request, DesktopForm)
+
+
+def add_mobile(request):
+    return add_device(request, MobileForm)
