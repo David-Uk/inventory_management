a0466cfbfcc862a275280088d0cfbc230ccfb7c0
David Ukelere <david.ukelere291@gmail.com>
2020-05-21 14:48:44 +0100

    Edit feature added

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Laptop, Desktop, Mobile
 from .forms import *
 
@@ -58,3 +58,29 @@ def add_desktop(request):
 
 def add_mobile(request):
     return add_device(request, MobileForm)
+
+
+def edit_item(request, pk, model, cls):
+    item = get_object_or_404(model, pk=pk)
+
+    if request.method == "POST":
+        form = cls(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = cls(instance=item)
+
+        return render(request, 'edit_item.html', {'form': form})
+
+
+def edit_laptop(request, pk):
+    return edit_item(request, pk, Laptop, LaptopForm)
+
+
+def edit_desktop(request, pk):
+    return edit_item(request, pk, Desktop, DesktopForm)
+
+
+def edit_mobile(request, pk):
+    return edit_item(request, pk, Mobile, MobileForm)
